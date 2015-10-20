6be5c7c200a565f99d9dfea3d85f1ec4a0a5aec3
RM <redbird_project@yahoo.fr>
2015-10-20 22:33:22 +0200

    Add a Slack WebHook helpers

---
+++ aspnet-mvc-helpers/SlackWebHookHelpers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace aspnet_mvc_helpers
+{
+    /// <summary>
+    /// Helpers to send message to slack
+    ///  look at https://api.slack.com/incoming-webhooks for more information
+    /// </summary>
+    public static class SlackWebHookHelpers
+    {
+        /// <summary>
+        /// Send a simple text message to slack
+        ///  All other parameters is defaults
+        /// </summary>
+        /// <param name="webHookSlackUrl">Url provided by Slack on Incoming WebHooks page</param>
+        /// <param name="message">Message to send</param>
+        ///  <returns>200 : Ok, other : Http Status code of error</returns>
+        public static HttpStatusCode Send(string webHookSlackUrl, string message)
+        {
+            var info = new PayLoad { Text = message };
+            return Send(webHookSlackUrl, info);
+        }
+
+        /// <summary>
+        /// Send a full qualified object to Slack
+        /// </summary>
+        /// <param name="webHookSlackUrl">Url provided by Slack on Incoming WebHooks page</param>
+        /// <param name="infoLoad"></param>
+        ///  <returns>200 : Ok, other : Http Status code of error</returns>
+        public static HttpStatusCode Send(string webHookSlackUrl, PayLoad infoLoad)
+        {
+            if (string.IsNullOrEmpty(webHookSlackUrl))
+                throw new ArgumentNullException(nameof(webHookSlackUrl));
+
+            var jsonToSend = JsonConvert.SerializeObject(infoLoad,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    StringEscapeHandling = StringEscapeHandling.EscapeNonAscii
+                });
+
+
+            // init rest client with Slack Url
+            var client = new RestClient(webHookSlackUrl);
+
+            // set the method : POST for slack
+            var request = new RestRequest(Method.POST);
+            // set the payload param
+            request.AddParameter("payload", jsonToSend);
+
+            // and execute it
+            var response = client.Execute(request);
+            // return Http Status of call
+            return response.StatusCode;
+        }
+    }
+
+    /// <summary>
+    /// Class to serialize in JSON for send to Slack
+    /// </summary>
+    public class PayLoad
+    {
+        //  'payload={"channel": "#newuser", "username": "webhookbot", "text": "This is posted to #newuser and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' https://hooks.slack.com/services/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
+        /// <summary>
+        /// Channel where you want to publish
+        /// </summary>
+        [JsonProperty(PropertyName = "channel")]
+        public string Channel { get; set; }
+        /// <summary>
+        /// In the name of
+        /// </summary>
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+        /// <summary>
+        /// Message you want to public
+        /// </summary>
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+        /// <summary>
+        ///  Emoji code
+        /// </summary>
+        [JsonProperty(PropertyName = "icon_emoji")]
+        public string IconEmoji { get; set; }
+        /// <summary>
+        /// Url of message icon 
+        /// </summary>
+        [JsonProperty(PropertyName = "icon_url")]
+        public string IconUrl { get; set; }
+    }
+}
